8b6bed6d28fbf16ba13aa68ab1f52aa55a383e04
valentin <valentin@rankingalive.com>
2013-03-05 16:45:29 -0800

    Fusion helpers of bootstrap_helper gem

--- lib/twitter/bootstrap/rails/engine.rb
+++ lib/twitter/bootstrap/rails/engine.rb
@@ -5,26 +5,27 @@ require File.dirname(__FILE__) + '/../../../../app/helpers/flash_block_helper.rb
 require File.dirname(__FILE__) + '/../../../../app/helpers/modal_helper.rb'
 
 module Twitter
-  module Bootstrap
-    module Rails
-      class Engine < ::Rails::Engine
-        initializer 'twitter-bootstrap-rails.setup',
-          :after => 'less-rails.after.load_config_initializers',
-          :group => :all do |app|
-            if defined?(Less)
-              app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
-            end
-          end
+    module Bootstrap
+        module Rails
+            class Engine < ::Rails::Engine
+                initializer 'twitter-bootstrap-rails.setup',
+                  :after => 'less-rails.after.load_config_initializers',
+                  :group => :all do |app|
+                    if defined?(Less)
+                        app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
+                    end
+                end
 
-        initializer 'twitter-bootstrap-rails.setup_helpers' do |app|
-          app.config.to_prepare do
-            ActionController::Base.send :include, BreadCrumbs
-            ActionController::Base.send :helper, FlashBlockHelper
-            ActionController::Base.send :helper, ModalHelper
-            #ActionController::Base.send :helper_method, :render_breadcrumbs
-          end
+                initializer 'twitter-bootstrap-rails.setup_helpers' do |app|
+                    ActiveSupport.on_load :action_controller do
+                        include BreadCrumbs
+                    end
+                    ActionView::Base.send :include, ModalHelper
+                    ActionView::Base.send :include, MiscHelper
+                    ActionView::Base.send :include, FlashBlockHelper
+                    ActionView::Base.send :include, BootstrapFlashHelper
+                end
+            end
         end
-      end
     end
-  end
 end
